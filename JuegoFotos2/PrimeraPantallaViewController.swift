976e901c0abef5a05e9f6bb4516dc5952fb82b77
Joselu Mac <escrita15@gmail.com>
2024-10-16 10:33:11 +0200

    Initial Commit

---
+++ JuegoFotos2/PrimeraPantallaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuegoFotos2
+//
+//  Created by José Luis on 16/10/24.
+//
+
+import UIKit
+
+class PrimeraPantallaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
